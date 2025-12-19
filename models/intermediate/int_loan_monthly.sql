554c07ab9572bb719229b56e059573d086e9400c
Saayed Alam <saayedalam@gmail.com>
2025-12-19 23:22:45 +0000

    added intermediate

---
+++ models/intermediate/int_loan_monthly.sql
@@ -0,0 +1,22 @@
+{{ config(materialized='view') }}
+
+select
+  p.loan_sequence_number,
+  p.monthly_reporting_period,
+  p.current_actual_upb,
+  p.current_loan_delinquency_status,
+  p.current_interest_rate,
+  p.loan_age,
+  a.credit_score,
+  a.first_payment_date,
+  a.maturity_date,
+  a.orig_upb,
+  a.orig_interest_rate,
+  a.property_state,
+  a.property_type,
+  a.channel,
+  a.seller_name,
+  a.servicer_name
+from {{ ref('stg_freddie_performance') }} p
+left join {{ ref('stg_freddie_acquisition') }} a
+  on p.loan_sequence_number = a.loan_sequence_number
